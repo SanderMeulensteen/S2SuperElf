302ceea3757c3570dfdfdee446ba19987b3a2195
SandersonMeulensteen <63857652+SanderMeulensteen@users.noreply.github.com>
2021-09-20 16:08:49 +0200

    Lagen&Klassen
    Lagen en Klassen toegevoegd aan het project

---
+++ SuperElf/Logic_SuperElf/Enumerations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Logic_SuperElf
+{
+    public enum Role
+    {
+        Admin, User
+    }
+
+    public enum Formation
+    {
+        ThreeFourThree, FourThreeThree, FourFourTwo
+    }
+    public enum Position
+    {
+        Goalkeeper, Defender, Midfielder, Striker
+    }
+    public enum Club
+    {
+        Ajax, AZ, FCGroningen, FCTwente, FCUtrecht, Feyenoord, FortunaSittard, GoAheadEagles, HeraclesAlmelo, NEC, PECZwolle, PSV, RKCWaalwijk, SCCambuurLeeuwarden, SCHeerenveen, SpartaRotterdam, Vitesse, WillemII
+    }
+}
