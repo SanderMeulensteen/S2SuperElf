15a8fe0005a8e1a9bc8f6522eb5775461d7ea22f
SandersonMeulensteen <63857652+SanderMeulensteen@users.noreply.github.com>
2021-12-24 11:19:51 +0100

    My team changes

--- SuperElf/UI_SuperElf/Controllers/HomeController.cs
+++ SuperElf/UI_SuperElf/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Logic_Factories_SuperElf;
+using Logic_Interfaces_SuperElf;
+using Microsoft.AspNetCore.Http;
 using UI_SuperElf.Models;
 
 namespace UI_SuperElf.Controllers
@@ -12,17 +15,24 @@ namespace UI_SuperElf.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUser _user = User_Factory.CreateUser();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public ActionResult Index()
         {
             try
             {
-                return View();
+                HomeViewModel home = new HomeViewModel();
+                int? sessionId = HttpContext.Session.GetInt32("_SessionUserId");
+                string sessionIdString = sessionId.ToString();
+                int id = Int32.Parse(sessionIdString);
+                List<IPlayer> players = _user.GetPlayersFromTeam(id);
+                home.teamOfTheWeek = players.OrderBy(x => x.position).ToList();
+                return View(home);
             }
             catch
             {
---
+++ SuperElf/UI_SuperElf/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Logic_Interfaces_SuperElf;
+
+namespace UI_SuperElf.Models
+{
+    public class HomeViewModel
+    {
+        public List<IPlayer> teamOfTheWeek = new List<IPlayer>();
+    }
+}
